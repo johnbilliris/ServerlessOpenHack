9f865ba4fcc8d6572b4f20f6533b6b02b7b6b0e4
Fai Lai <hoongfai@microsoft.com>
2020-10-20 23:41:51 +1100

    implemented GetRating using CosmosDB binding

--- GetRating.cs
+++ GetRating.cs
@@ -14,22 +14,27 @@ namespace Team1
     {
         [FunctionName("GetRating")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "ratingsdata",
+                collectionName: "ratings",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "{Query.ratingId}",
+                PartitionKey = "{Query.userId}")] RatingClass rating,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("GetRating HTTP trigger function processed a request");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            if (rating == null)
+            {
+                log.LogInformation($"Rating item not found");
+                return new BadRequestResult();
+            }
+            else
+            {
+                log.LogInformation($"Found Rating item, Description={rating.Id}");
+                return new OkObjectResult(rating);
+            }
         }
     }
 }
